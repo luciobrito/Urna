3050653f961782cd1f26a14bc461cb5e32a3291b
Lucio <kingdomlink69@gmail.com>
2023-04-10 18:22:57 -0300

    Melhorias na exibição do resultado

--- votacao/resultado.php
+++ votacao/resultado.php
@@ -20,9 +20,15 @@ while($row = mysqli_fetch_assoc($resu2)){
 $contas = array_count_values($votos);
 //Filtra os valores unicos na array
 $unico = array_unique($votos);
+//Compara os valores entre candidatos registrados e candidatos votados
+$nulo = array_diff($unico,$num_cand);
+//Coloca os votos nulos em ordem crescente;
+sort($nulo);
+//Conta a quantidade de valores nulos existentes.
+$qnulo = count($nulo);
+
 //Coloca os valores em ordem crescente
 sort($unico);
-echo var_dump($contas);
 echo "<br>";
 //Conta quantos valores unicos existem na array;
 
@@ -32,6 +38,8 @@ for($i = 0; $i < $nunico; $i++){
     echo " Partido $unico[$i] | Votos:   " . $contas[$unico[$i]] . "<br>";
 
 }
+echo "<br> Brancos e Nulos: " . $qnulo;
 //Conta a quantidade de valores existentes na array;
-echo "<br> Votos totais:" . count($votos);
+echo "<br> Votos totais:" . count($votos)."<br>";
 #echo "Partido $unico[0] | Votos:" . $contas[$unico[0]] . "<br> Partido $unico[1] | Votos: ". $contas[$unico[1]] ?? 0; echo "<br>Total: " . count($votos);
+echo  var_dump($num_cand);
